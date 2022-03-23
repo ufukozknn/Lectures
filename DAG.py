d4b2ac048bcb057e2c3932ec3aebc162939436c2
Ali Tamer Ünal <atu@icrontech.com>
2022-03-23 14:25:00 +0300

    added class skeletons.

--- DAG.py
+++ DAG.py
@@ -1,6 +1,29 @@
 
 # not object oriented!!
 
+class DAG:
+    def __init__(self) -> None:
+         nodes = []
+         arcs = []
+
+         nodes.append(Node(1))
+         nodes.append(Node(2))         
+         nodes.append(Node(3))
+
+         arcs.append(nodes[0], nodes[1], 7)
+         arcs.append(nodes[0], nodes[2], 10)
+         arcs.append(nodes[1], nodes[2], 5)
+
+class Node:
+    def __init__(self, code: str) -> None:
+        self.code = code
+    
+
+class Arc:
+    def __init__(self, from_node: Node, to_node: Node, distance: int) -> None:
+        self.from_node = from_node
+        self.to_node = to_node
+        self.distance = distance
 
 
 
